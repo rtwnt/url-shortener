d5f87e7b1981a7046e4820923b4e6ace83f1b05a
Piotr Rusin <piotr.rusin88@gmail.com>
2016-05-02 11:49:46 +0200

    Add Slug class

--- test/unit/test_url_shortener.py
+++ test/unit/test_url_shortener.py
@@ -2,6 +2,71 @@
 
 import unittest
 
+from nose_parameterized import parameterized
+
+from url_shortener import Slug
+
+
+class SlugTest(unittest.TestCase):
+    ''' Tests for Slug class
+
+    :var SLUG_INTEGER: maps string values of slugs used in tests to
+    corresponding integers.
+    :var INTEGERS: a list of lists, each containing one of the integers
+    specified in SLUG_INTEGER. Used as an argument for
+    parameterized.expand()
+    :var SLUGS_TO_INTEGERS: a list containing tuples, each with a pair of
+    slug string and its corresponding integer. Used as an argument for
+    parameterized.expand()
+    '''
+    SLUG_INTEGER = {
+        'baa': 1*3**2,
+        'ccb': 2*3**2+2*3+1,
+        'caca': 2*3**3+2*3,
+        'ccc': 2*3**2+2*3+2,
+        'a': 0
+    }
+    INTEGERS = [[v] for v in SLUG_INTEGER.values()]
+    SLUGS_TO_INTEGERS = SLUG_INTEGER.items()
+
+    @classmethod
+    def setUpClass(cls):
+        Slug.CHARS = 'abc'
+        Slug.BASE = 3
+
+    def test_init_for_invalid_string(self):
+        ''' The string contains forbidden characters '''
+        string = 'abcd'
+        self.assertRaises(ValueError, Slug, None, string)
+
+    def test_init_for_invalid_arg_set(self):
+        ''' The arguments integer and string can't both be None '''
+        self.assertRaises(ValueError, Slug, None, None)
+
+    @parameterized.expand(SLUGS_TO_INTEGERS)
+    def test_init_for_generated_integer(self, string, expected_integer):
+        ''' The integer value of slug should be properly calculated
+        when integer == None '''
+        instance = Slug(string=string)
+        actual_integer = instance.integer
+        self.assertEqual(expected_integer, actual_integer)
+
+    @parameterized.expand(INTEGERS)
+    def test_init_for_string_being_none(self, integer):
+        ''' The string value should not be generated during
+        object initialization'''
+        instance = Slug(integer=integer)
+        self.assertIsNone(instance._string)
+
+    @parameterized.expand(SLUGS_TO_INTEGERS)
+    def test_str_for_string_being_none(self, expected_string, integer):
+        ''' The __str__ method should properly generate string
+        based on objects .integer property '''
+        instance = Slug(integer=integer)
+        actual_string = str(instance)
+        self.assertEqual(expected_string, actual_string)
+
+
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
--- url_shortener.py
+++ url_shortener.py
@@ -1,4 +1,80 @@
 # -*- coding: utf-8 -*-
 
+from bisect import bisect_left
+
+
+class Slug(object):
+    ''' An identifier for shortened url
+
+    In has two values used as its representations: a string value
+    and an integer value, used in short urls and in database,
+    respectively.
+
+    :var CHARS: string containing characters allowed to be used
+    in a slug. The characters are used as digits of a numerical system
+    used to convert between the string and integer representations.
+    :var BASE: a base of numeral system used to convert between
+    the string and integer representations.
+    '''
+
+    CHARS = '0123456789abcdefghijkmnopqrstuvwxyz'
+    BASE = len(CHARS)
+
+    def __init__(self, integer=None, string=None):
+        ''' Initialize new instance
+
+        :param integer: a value representing the slug as an integer.
+        It can not be None while string is None. If it is None, a
+        corresponding property of the object will be based on
+        the string parameter
+        :param string: a value representing the slug as a string.
+        It can not be None while integer is None, and it has to consist
+        only of characters specified by the CHARS class property.
+        If it is None, a value of corresponding property of the object
+        will be based on the integer parameter
+        :raises ValueError: if the slug contains characters that are not
+        in self.CHARS property, or if both string and integer params
+        are None
+        '''
+        if string is not None:
+            forbidden = [d for d in string if d not in self.CHARS]
+            if forbidden:
+                msg_tpl = "The slug '{}' contains forbidden characters: '{}'"
+                raise ValueError(msg_tpl.format(string, forbidden))
+        elif integer is None:
+            raise ValueError(
+                'The string and integer arguments cannot both be None'
+            )
+
+        self._string = string
+
+        self.integer = integer
+        if integer is None:
+            value = 0
+            for exponent, char in enumerate(reversed(string)):
+                digit_value = bisect_left(self.CHARS, char)
+                value += digit_value*self.BASE**exponent
+            self.integer = value
+
+    def __str__(self):
+        ''' Get string representation of the slug
+
+        :returns: a string representing value of the slug as a numeral
+        of base specified for the class. If the object has been
+        initialized with integer as its only representation,
+        the numeral will be derived from it using the base.
+        '''
+        if self._string is None:
+            value = ''
+            integer = self.integer
+            while True:
+                integer, remainder = divmod(integer, self.BASE)
+                value = self.CHARS[remainder] + value
+                if integer == 0:
+                    break
+            self._string = value
+        return self._string
+
+
 if __name__ == '__main__':
     pass
