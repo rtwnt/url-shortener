5fd085b920f49301cfdfa2f8a6612728a8fe5650
Piotr Rusin <piotr.rusin88@gmail.com>
2016-05-09 10:53:28 +0200

    Make event_handlers module use new alias length configuration

--- url_shortener/event_handlers.py
+++ url_shortener/event_handlers.py
@@ -6,9 +6,13 @@ SQLAlchemy event handlers
 from sqlalchemy import event
 
 from .models import Alias, ShortenedUrl
+from . import app
 
 
-random_alias_generator = Alias.random_factory(1, 4)
+random_alias_generator = Alias.random_factory(
+    app.config['MIN_NEW_ALIAS_LENGTH'],
+    app.config['MAX_NEW_ALIAS_LENGTH']
+)
 
 
 @event.listens_for(ShortenedUrl, 'before_insert')
