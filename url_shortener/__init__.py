503905c635a694e32939dd9cb796f2a10b37e7d9
Piotr Rusin <piotr.rusin88@gmail.com>
2016-05-09 09:15:04 +0200

    Add additional application configuration
    This commit adds external configuration file whose address is
    stored in an environment variable.

--- url_shortener/__init__.py
+++ url_shortener/__init__.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('url_shortener.default_config')
+app.config.from_envvar('URL_SHORTENER_CONFIGURATION')
 db = SQLAlchemy(app)
 
 from . import event_handlers
