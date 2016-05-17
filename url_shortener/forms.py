0ea4ffc885e071feabba1787133de1d0071372f8
Piotr Rusin <piotr.rusin88@gmail.com>
2016-05-17 13:28:30 +0200

    Add validators to ShortenedUrlForm

--- url_shortener/forms.py
+++ url_shortener/forms.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 from flask_wtf import Form
-from wtforms import StringField
+from wtforms import StringField, validators
 
 
 class ShortenedUrlForm(Form):
-    url = StringField('Url to be shortened')
+    url = StringField(
+        'Url to be shortened',
+        [
+            validators.DataRequired(),
+            validators.URL(message="A valid url is required")
+        ]
+    )
