125ee07c94f60d31ebe1420e11ae8f3ca91bc665
Piotr Rusin <piotr.rusin88@gmail.com>
2016-05-17 13:27:18 +0200

    Add log file setup to run_server module

--- run_server.py
+++ run_server.py
@@ -2,3 +2,10 @@
 from url_shortener import app
 
 app.run()
+
+if not app.debug:
+    import logging
+    from logging.handlers import TimedRotatingFileHandler
+    file_handler = TimedRotatingFileHandler(app.config['LOG_FILE'])
+    file_handler.setLevel(logging.WARNING)
+    app.logger.addHandler(file_handler)
