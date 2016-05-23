802cd487f3b28c865556fc404dff8beb1ce6b5bd
Piotr Rusin <piotr.rusin88@gmail.com>
2016-05-23 12:21:52 +0200

    Add not_spam validator to forms.ShortenedUrlForm

--- url_shortener/forms.py
+++ url_shortener/forms.py
@@ -2,12 +2,15 @@
 from flask_wtf import Form
 from wtforms import StringField, validators
 
+from .validation import not_spam
+
 
 class ShortenedUrlForm(Form):
     url = StringField(
         'Url to be shortened',
         [
             validators.DataRequired(),
-            validators.URL(message="A valid url is required")
+            validators.URL(message="A valid url is required"),
+            not_spam
         ]
     )
