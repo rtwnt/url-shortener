4968d4a4eeb4cb14d6bb206a6130c1a29f97c469
Piotr Rusin <piotr.rusin88@gmail.com>
2016-05-23 12:15:23 +0200

    Add instance of GoogleSafeBrowsing to url_shortener.validation

--- url_shortener/validation.py
+++ url_shortener/validation.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+from submodules.spam_lists_lib.spam_lists import GoogleSafeBrowsing
+
+from . import app
+
+google_safe_browsing = GoogleSafeBrowsing(
+    'url-shortener',
+    '0.9',
+    app.config['GOOGLE_SAFE_BROWSING_API_KEY']
+)
