d2321feff865c447e1a7875a0c7260b83847a2ef
Piotr Rusin <piotr.rusin88@gmail.com>
2016-05-23 12:16:23 +0200

    Add instance of HpHosts to url_shortener.validation

--- url_shortener/validation.py
+++ url_shortener/validation.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from submodules.spam_lists_lib.spam_lists import GoogleSafeBrowsing
+from submodules.spam_lists_lib.spam_lists import GoogleSafeBrowsing, HpHosts
 
 from . import app
 
@@ -8,3 +8,5 @@ google_safe_browsing = GoogleSafeBrowsing(
     '0.9',
     app.config['GOOGLE_SAFE_BROWSING_API_KEY']
 )
+
+hp_hosts = HpHosts('url-shortener')
