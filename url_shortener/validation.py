ab0b364818fd7a62ad8c286b9505a0bbf8dab3c6
Piotr Rusin <piotr.rusin88@gmail.com>
2016-05-23 12:17:07 +0200

    Add instance of GeneralizedUrlTester to url_shortener.validation

--- url_shortener/validation.py
+++ url_shortener/validation.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-from submodules.spam_lists_lib.spam_lists import GoogleSafeBrowsing, HpHosts
+from submodules.spam_lists_lib.spam_lists import (
+    GoogleSafeBrowsing, HpHosts, GeneralizedUrlTester, UrlTesterChain,
+    SPAMHAUS_DBL, SPAMHAUS_ZEN, SURBL_MULTI
+)
 
 from . import app
 
@@ -10,3 +13,12 @@ google_safe_browsing = GoogleSafeBrowsing(
 )
 
 hp_hosts = HpHosts('url-shortener')
+spam_tester = GeneralizedUrlTester(
+    UrlTesterChain(
+        SPAMHAUS_DBL,
+        SPAMHAUS_ZEN,
+        SURBL_MULTI,
+        hp_hosts,
+        google_safe_browsing
+    )
+)
