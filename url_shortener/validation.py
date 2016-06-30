fce66ed1d069a3ea54831cf1f5180dd7d79a3660
Piotr Rusin <piotr.rusin88@gmail.com>
2016-06-30 11:48:43 +0200

    Add get_msg_if_blacklisted_or_spam function to validation module

--- url_shortener/validation.py
+++ url_shortener/validation.py
@@ -57,6 +57,24 @@ blacklist_tester = GeneralizedUrlTester(
 )
 
 
+def get_msg_if_blacklisted_or_spam(url):
+    ''' Get a message if given url has blacklisted host
+    or is recognized as spam
+
+    :param url: a url value to be tested
+    :returns: a string message if any of the testers recognize
+    the url as match, or None
+    '''
+    msg_map = {
+        blacklist_tester: 'This value is blacklisted',
+        spam_tester: 'This value has been recognized as spam'
+    }
+
+    for tester, msg in msg_map.items():
+        if tester.any_match([url]):
+            return msg
+
+
 def not_blacklisted_nor_spam(form, field):
     ''' Check if the data in the field is not
     blacklisted nor a spam
