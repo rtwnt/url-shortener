2f3c1c44e2cdb8586cbd4f33374e87ab006445f4
Piotr Rusin <piotr.rusin88@gmail.com>
2016-07-05 14:08:07 +0200

    Add project metadata attributes to run_server module

--- run_server.py
+++ run_server.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from url_shortener import app, event_handlers, views
 
+__title__ = 'url-shortener'
+__version__ = '0.9.0.dev1'
+__author__ = 'Piotr Rusin'
+__email__ = "piotr.rusin88@gmail.com"
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2016 Piotr Rusin'
+
 
 if not app.debug:
     import logging
