80e519664ff03be6993f6a1af706ded0603a27fe
Piotr Rusin <piotr.rusin88@gmail.com>
2016-08-30 11:32:28 +0200

    Add validation message for DataRequired validator

--- url_shortener/forms.py
+++ url_shortener/forms.py
@@ -9,7 +9,7 @@ from .validation import not_blacklisted_nor_spam
 class ShortenedURLForm(Form):
     url = StringField(
         validators=[
-            validators.DataRequired(),
+            validators.DataRequired(message='A target URL is required.'),
             validators.URL(message='A valid URL is required.'),
             not_blacklisted_nor_spam
         ],
