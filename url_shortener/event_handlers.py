8a961d9c6fc8d750eed6358f571fa54c105e0fbb
Piotr Rusin <piotr.rusin88@gmail.com>
2016-09-24 13:50:36 +0200

    Refactor url_shortener.even_handlers module
    This commit refactors the module so it defines a function that calls
    Alias.create_random. The call to Alias.random_factory and to the function
    returned by it is being removed.

--- url_shortener/event_handlers.py
+++ url_shortener/event_handlers.py
@@ -6,15 +6,8 @@ SQLAlchemy event handlers
 from sqlalchemy import event
 
 from .models import Alias, ShortenedURL
-from . import app
-
-
-random_alias_generator = Alias.random_factory(
-    app.config['MIN_NEW_ALIAS_LENGTH'],
-    app.config['MAX_NEW_ALIAS_LENGTH']
-)
 
 
 @event.listens_for(ShortenedURL, 'before_insert')
 def assign_alias_before_insert(mapper, connection, target):
-    target.alias = random_alias_generator()
+    target.alias = Alias.create_random()
