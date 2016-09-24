f72edccc731baac72d9764a8845429704178796c
Piotr Rusin <piotr.rusin88@gmail.com>
2016-09-24 14:06:03 +0200

    Remove Alias.random_factory
    The method removed by this commit is no longer used.

--- url_shortener/models.py
+++ url_shortener/models.py
@@ -244,63 +244,6 @@ class Alias(object):
             self._string = self._SYSTEM.to_string(self.integer)
         return self._string
 
-    @classmethod
-    def random_factory(cls, min_length, max_length):
-        """ Get a function returning new instances of the class
-        with a random integer as argument
-
-        The arguments provide a range of lengths for string
-        representations of instances of Alias to be returned
-        by the factory. For these values, the method calculates
-        a range of integer values corresponding to them.
-
-        From that range, only integers smaller than maximum
-        32-bit integer can be used to generate new aliases.
-
-        :param min_length: a minimum number of characters for string
-        representation of the instances returned by the factory
-        :param max_length: a maximum number of characters for string
-        representation of the instances returned by the factory
-        :raises AliasLengthValueError: if:
-
-        * values of any of the parameters are less than zero, or
-        * max_length > min_length, or
-        * the whole range of alias string lengths, or just part
-        of it, is not available for generation due to integer values
-        corresponding to it being larger than maximum 32-bit
-        signed integer.
-
-        This value is assumed as maximum allowed for the integers used
-        in generation because we assume SQLAlchemy.types.Integer -
-        a base type for alias property of ShortenedURL class - will
-        translate into 32 bit signed integer type of underlying
-        database engine used by the application.
-        :returns: a function returning instances of the class
-        based on random integers with a pre-calculated range
-        """
-
-        if not 0 < min_length <= max_length:
-            raise AliasLengthValueError('The length limits are incorrect')
-        max_int_32 = 2**31 - 1
-        min_integer = cls._SYSTEM.get_min_value(min_length)
-        if min_integer > max_int_32:
-            raise AliasLengthValueError(
-                'The minimum length of a new alias is too large'
-            )
-
-        if (min_length < max_length and
-                cls._SYSTEM.get_min_value(max_length) > max_int_32):
-            raise AliasLengthValueError(
-                'The maximum length of a new alias is too large'
-            )
-
-        max_integer = min(max_int_32, cls._SYSTEM.get_max_value(max_length))
-
-        def factory():
-            random_integer = randint(min_integer, max_integer)
-            return cls(integer=random_integer)
-        return factory
-
 
 class IntegerAlias(types.TypeDecorator):
     """ Converts between database integers and
