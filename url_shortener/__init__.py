36ba4fc0508de076ff5a9c701b30a942be86aa1d
Piotr Rusin <piotr.rusin88@gmail.com>
2016-09-28 20:00:11 +0200

    Improve a docstring in url_shortener/__init__.py
    The docstring was largely unnecessary. I think it is enough to include just
    a link to documentation to provide an explanation for the following line.

--- url_shortener/__init__.py
+++ url_shortener/__init__.py
@@ -25,18 +25,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('url_shortener.default_config')
-"""
-From http://flask-sqlalchemy.pocoo.org/2.1/config/:
 
-"SQLALCHEMY_TRACK_MODIFICATIONS - If set to True, Flask-SQLAlchemy
-will track modifications of objects and emit signals. The default is
-None, which enables tracking but issues a warning that it will be
-disabled by default in the future. This requires extra memory and
-should be disabled if not needed."
-
-This application uses SQLAlchemy event system, so this value is set
-to False
-"""
+# See http://flask-sqlalchemy.pocoo.org/2.1/config/
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config.from_envvar('URL_SHORTENER_CONFIGURATION')
 db = SQLAlchemy(app)
