5f409fd075e1bf4d4d58cb280f25761965f6a446
Piotr Rusin <piotr.rusin88@gmail.com>
2016-09-30 21:48:53 +0200

    Refactor applying custom configuration after it is loaded
    The call sending custom_config_loaded is being replaced with direct
    calls to the functions configure_random_factory and configure_url_validator.

--- url_shortener/__main__.py
+++ url_shortener/__main__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-from url_shortener import app, custom_config_loaded, views
+from url_shortener import app, views
+from url_shortener.validation import configure_url_validator
+from url_shortener.models import configure_random_factory
 
 log_file = app.config['LOG_FILE']
 
@@ -11,6 +13,8 @@ if not app.debug and log_file is not None:
     app.logger.addHandler(file_handler)
 
 app.config.from_envvar('URL_SHORTENER_CONFIGURATION')
-custom_config_loaded.send(app)
+configure_url_validator(app)
+configure_random_factory(app)
+
 
 app.run()
