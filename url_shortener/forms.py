45f974059ff89f4c6ca40361131368f6e2471f1d
Piotr Rusin <piotr.rusin88@gmail.com>
2016-10-21 12:15:35 +0200

    Remove ShortenedURLForm class

--- url_shortener/forms.py
+++ url_shortener/forms.py
@@ -4,26 +4,7 @@ from flask_wtf.recaptcha import RecaptchaField, Recaptcha
 from injector import Module, provider, Key, singleton
 from wtforms import StringField, validators
 
-from .validation import url_validator, BlacklistValidator
-
-
-class ShortenedURLForm(FlaskForm):
-    url = StringField(
-        validators=[
-            validators.DataRequired(message='A target URL is required.'),
-            validators.URL(message='A valid URL is required.'),
-            url_validator.assert_not_blacklisted
-        ],
-        render_kw={'placeholder': 'Original URL'}
-    )
-    recaptcha = RecaptchaField(
-        validators=[
-            Recaptcha(
-                'Please click on the reCAPTCHA field '
-                'to prove you are a human.'
-            )
-        ]
-    )
+from .validation import BlacklistValidator
 
 
 url_form_class = Key('url_form_class')
