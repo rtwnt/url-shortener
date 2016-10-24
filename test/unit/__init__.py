8ed21427eeebceb72578466e626527685b65b3b0
Piotr Rusin <piotr.rusin88@gmail.com>
2016-10-24 15:54:58 +0200

    Remove needless code from test/unit/__init__.py
    The HOST_BLACKLIST_FILE option is no longer used by the application,
    so the code responsible for setting its value for running tests is no
    longer necessary.

--- test/unit/__init__.py
+++ test/unit/__init__.py
@@ -1,10 +1,1 @@
 # -*- coding: utf-8 -*-
-from url_shortener import app
-
-""" Host blacklist file is unnecessary during testing.
-It doesn't exist in the directory structure of test package,
-so attempting to load it during testing would cause FileNotFoundError.
-
-We set the file name to None to prevent that.
-"""
-app.config['HOST_BLACKLIST_FILE'] = None
