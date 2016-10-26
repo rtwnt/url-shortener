8142a48a10799f83d6d8a0d29c50afccd5e9fd68
Piotr Rusin <piotr.rusin88@gmail.com>
2016-10-26 15:22:16 +0200

    Add get_app_and_db function

--- url_shortener/__init__.py
+++ url_shortener/__init__.py
@@ -26,7 +26,7 @@ from flask_injector import FlaskInjector
 
 from .views import url_shortener
 from .forms import FormModule
-from .domain_and_persistence import DomainAndPersistenceModule
+from .domain_and_persistence import DomainAndPersistenceModule, SQLAlchemy
 from .validation import ValidationModule
 
 app = Flask(__name__)
@@ -65,3 +65,32 @@ def _get_injector(app):
         ],
         use_annotations=True
     )
+
+
+def get_app_and_db(configuration, from_envvar=False):
+    """Get application instance and database object used by it
+
+    :param configuration: a string value referring to a file from which
+    configuration options will be loaded. This value may be either
+    the name of the file, or name of an environment variable set to
+    the name of configuration file
+    :param from_envvar: if True: configuration parameter will
+    be treated as name of an evnironment variable pointing to
+    the configuration file, if False: it will be treated as name of
+    the configuration file itself.
+    :returns: a tuple with application object as its first and
+    database object as its second element
+    """
+
+    app = Flask(__name__)
+    app.config.from_object('url_shortener.default_config')
+    if from_envvar:
+        app.config.from_envvar(configuration)
+    else:
+        app.config.from_pyfile(configuration)
+
+    _set_up_logging(app)
+    app.register_blueprint(url_shortener)
+    injector = _get_injector(app)
+
+    return app, injector.injector.get(SQLAlchemy)
